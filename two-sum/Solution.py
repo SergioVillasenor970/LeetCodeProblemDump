2eec7f80e05447a71bb8fba1b22ccf0a288eae0b
Sergio Villaseñor <sergiovillasenorparra@gmail.com>
2025-05-31 21:15:42 +0200

    Problem two-sum

---
+++ two-sum/Solution.py
@@ -0,0 +1,8 @@
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for index0 in range(len(nums)):
+            objetive = target - nums[index0]
+            if objetive in nums and nums.index(objetive) != index0:
+                return [index0, nums.index(objetive)]
+        return []
